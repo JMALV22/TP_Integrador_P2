1afcf2918c7931902f347a8d64d61562d1e4ed10
Luciano <andreloluciano91@gmail.com>
2025-11-06 16:43:30 -0300

    agregada funcion obtenerporid

--- src/dao/PacienteDaoImpl.java
+++ src/dao/PacienteDaoImpl.java
@@ -14,7 +14,7 @@ public class PacienteDaoImpl implements PacienteDao {
     public List<Paciente> obtenerTodos(Connection con) throws SQLException {
         String sql = "SELECT id_paciente, nombre, apellido, dni, fecha_nacimiento, eliminado " +
                 "FROM paciente " +
-                "WHERE eliminado = FALSE " +
+                "WHERE eliminado = FALSE " + // los eliminados no saldrian en el resultado, pero siguen en la db
                 "ORDER BY id_paciente";
 
         List<Paciente> lista = new ArrayList<>();
@@ -22,7 +22,7 @@ public class PacienteDaoImpl implements PacienteDao {
         try (PreparedStatement ps = con.prepareStatement(sql);
              ResultSet rs = ps.executeQuery()) {
 
-            while (rs.next()) {
+            while (rs.next()) { // lee columna por columna
                 Paciente p = new Paciente();
                 p.setId(rs.getLong("id_paciente"));
                 p.setNombre(rs.getString("nombre"));
@@ -51,7 +51,38 @@ public class PacienteDaoImpl implements PacienteDao {
 
     @Override
     public Paciente obtenerPorId(Long id, Connection con) throws SQLException {
-        throw new UnsupportedOperationException("obtenerPorId() aún no implementado");
+        String sql = "SELECT id_paciente, nombre, apellido, dni, fecha_nacimiento, eliminado " +
+                "FROM paciente " +
+                "WHERE id_paciente = ? AND eliminado = FALSE";
+
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setLong(1, id); // reemplaza el ? con el valor recibido
+
+            try (ResultSet rs = ps.executeQuery()) {
+
+                if (rs.next()) { // si encontro un registro
+
+                    Paciente p = new Paciente();
+                    p.setId(rs.getLong("id_paciente"));
+                    p.setNombre(rs.getString("nombre"));
+                    p.setApellido(rs.getString("apellido"));
+                    p.setDni(rs.getString("dni"));
+
+                    Date f = rs.getDate("fecha_nacimiento");
+                    if (f != null) {
+                        p.setFechaNacimiento(f.toLocalDate()); // se convierte a localdate
+                    }
+
+                    p.setEliminado(rs.getBoolean("eliminado"));
+
+                    return p; // devolvemos el objeto ya cargado
+                }
+            }
+        }
+
+        return null; // si no encontró ningún paciente con ese id, devuelve null
+
     }
 
     @Override
